9bddcb0b257811ee025dcfa26194c3e576b9cae3
luiz123o <luiz123o@hotmail.com>
2021-04-13 16:37:36 -0300

    att

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Countdown } from 'components/Countdown'
 import Profile from 'components/Profile'
 import ChallengeBox from 'components/ChallengeBox'
 import * as S from 'styles/pages/Home/styles'
-import Head from 'next/Head'
+import Head from 'next/head'
 import ExperienceBar from '../components/Experience'
 
 import { CountdownProvider } from 'contexts/CountdownContext'
